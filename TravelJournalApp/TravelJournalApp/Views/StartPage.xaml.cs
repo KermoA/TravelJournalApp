27c7f7b5712e0b47e30c1fd6b1a52c62123c8bb5
ccaups <ccaups@gmail.com>
2024-10-08 00:24:32 +0300

    Making fun! xD

--- TravelJournalApp/TravelJournalApp/Views/StartPage.xaml.cs
+++ TravelJournalApp/TravelJournalApp/Views/StartPage.xaml.cs
@@ -39,28 +39,33 @@ public partial class StartPage : ContentPage
 
         var parentAnimation = new Animation();
 
-        // Fade-in animation for opacity
-        parentAnimation.Add(0, 0.1, new Animation(v => imgTest.Opacity = v, 0, 1, Easing.CubicIn));
-
         // Get screen width and height dynamically
         double screenWidth = 540;
         double screenHeight = 1000;
 
         // Airplane image width, assuming it's already sized
-        double imgWidth = imgTest.WidthRequest;
-        double imgHeight = imgTest.HeightRequest;
+        double imgWidth = imgAirplane.WidthRequest;
+        double imgHeight = imgAirplane.HeightRequest;
 
+        // ??
         parentAnimation.Add(0, 0.4, new Animation(v => imgIcon.Opacity = v, 0, 1, Easing.CubicIn));
-        // First phase: Move the airplane image to the right, horizontally
         parentAnimation.Add(0.1, 1, new Animation(v =>
         {
-            // Liiguta vasakule
-            imgTest.TranslationX = screenWidth - v * 1.7;
+            // Moving to left
+            imgAirplane.TranslationX = screenWidth - v * 1.7;
 
-            // Liiguta üles
-            imgTest.TranslationY = screenHeight + imgHeight - v * 3.4 ;
+            // Moving to up
+            imgAirplane.TranslationY = screenHeight + imgHeight - v * 3.4;
         }, 0, screenWidth + imgWidth, Easing.Linear));
 
+        //Train moving
+        parentAnimation.Add(0, 1, new Animation(v =>
+        {
+            // Moving to right
+            imgTrain.TranslationX = 4 * v - 1000;
+
+
+        }, 0, screenWidth, Easing.Linear));
         parentAnimation.Commit(this, "TransitionAnimation", 10, 4000, null , null);
 
         await TravelMain();
@@ -68,10 +73,10 @@ public partial class StartPage : ContentPage
 
     async Task TravelMain()
     {
-        // Oota 10 sekundit
+        // Wait 5 seconds
         await Task.Delay(5000);
 
-        // Suuna leht ümber
+        // Redirecting page
         Application.Current.MainPage = new NavigationPage(new Main());
     }
 }
